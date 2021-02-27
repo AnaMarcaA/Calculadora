184325b9c4681207f49bdfc6577e5be8cf51b62d
Ana Marca <a2019113660@estudiantes.upsa.edu.bo>
2021-02-27 10:08:22 -0400

    Se añadio calculadora 2

---
+++ SolCalculadora2/APICalculadora2/Controllers/CalculadoraController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICalculadora2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public int Add([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa + numb;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int Multiply([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa * numb;
+        }
+
+        [HttpPost]
+        [Route("Subtracion")]
+        public int Subtraction([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa - numb;
+        }
+
+        [HttpPost]
+        [Route("Divide")]
+        public int Divide([FromHeader] int numa, [FromHeader] int numb)
+        {
+            return numa / numb;
+        }
+
+    }
+}
